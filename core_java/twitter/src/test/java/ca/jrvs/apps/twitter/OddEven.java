887ce88722745c912a99e2e292a3d3ff17ca0ca5
Hyebin Yoon <binyoon99@gmail.com>
2022-11-24 22:07:11 +0000

    Implemented HTTP&JSON features

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/OddEven.java
@@ -0,0 +1,13 @@
+package ca.jrvs.apps.twitter;
+
+
+public class OddEven {
+
+    public String oddEvenMod (int i){
+        return i % 2 == 0 ? "even" : "odd";
+    }
+
+    public String oddEvenBit(int i ){
+        return null;
+    }
+}
